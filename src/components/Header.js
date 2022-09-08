21d2df2253b2a086b77348336a9700f6324e7eef
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-09-08 18:54:26 +0400

    readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,6 @@ function Header() {
           React Movies
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li>
-            <a href="!#">Repo</a>
-          </li>
         </ul>
       </div>
     </nav>
